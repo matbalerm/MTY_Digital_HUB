6dc46c804ab210f5dabed9f6d75ba1a20e7ddbaa
matbalerm <matbalerm>
2022-09-06 21:58:29 +0000

    Tarea 2 completed.

---
+++ Tarea 2.py
@@ -0,0 +1,81 @@
+# Databricks notebook source
+x = 10
+print("Identidad:", id(x))
+print("Tipo:", type(x))
+print("Value:", x)
+
+# COMMAND ----------
+
+# Mutables: Si permiten ser modificados una vez creados.
+# Inmutables: Si no permiten ser modificados una vez creados.
+
+# COMMAND ----------
+
+l = (1,"Colombia",[1,2,3],True,1.5)
+
+# COMMAND ----------
+
+type(l)
+
+# COMMAND ----------
+
+l=list(l)
+
+# COMMAND ----------
+
+l[1]="Argentina"
+
+# COMMAND ----------
+
+l
+
+# COMMAND ----------
+
+l[2][0]
+
+# COMMAND ----------
+
+d1=[1,2,3,4,5,6]
+d2 = ['a','b','c']
+d1 = {}
+d3 = {}
+
+
+# COMMAND ----------
+
+for i in range(0,5):
+    d1[i+1] = l[i]
+
+d1
+
+for i in range(len(d2)):
+    d3[d2[i]] = l[i]
+
+d3
+
+
+
+# COMMAND ----------
+
+d3['c']
+
+# COMMAND ----------
+
+tarea1 = {}
+
+# COMMAND ----------
+
+for i in range(len(l)):
+    tarea1[i+1]=l[i]
+    
+tarea1
+
+# COMMAND ----------
+
+mylist = 'hello'
+x = len(mylist)
+x
+
+# COMMAND ----------
+
+
