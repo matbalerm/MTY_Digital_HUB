9eee965126d1989a2bd937ba52ebd54036c16cb3
matbalerm <matbalerm>
2022-09-14 01:35:29 +0000

    Actividad: Hobbies completed.

---
+++ Actividad: Hobbies.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+# MAGIC %md #### Actividad Avanzada: Procesos
+# MAGIC 
+# MAGIC Crea un algoritmo de un proceso dentro de tu empresa (venta, compras, ticket de cliente, etc.)
+
+# COMMAND ----------
+
+# Cálculo de Costo de Servir para México
+#1. Descargar las bases de costos desde BPC. 
+#2. Crear una subcarpeta con el rótulo 'Mes_Año_País' dentro de la carpeta de Costo de Servir.
+#3. Guardar dentro las bases descargadas.
+#4. Crear las 7 columnas adicionales para la clasificación de la base original.
+#5. Correr la macro de Clasificación.
+#6. Validar si hay excepciones.
+#7. En caso de existir, agregarlas manualmente de acuerdo a los lineamientos descritos en la 'Guía de Clasificación'
+#8. Guardar el documento y subirlo a la ruta especificada dentro de SharePoint.
+#9. Actualizar el PowerBI y validar que los datos hayan sido actualizados adecuadamente.
+#10. Publicar el tablero en la aplicación del país correspondiente. 
+
+# COMMAND ----------
+
+
