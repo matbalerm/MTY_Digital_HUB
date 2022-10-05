9728664229182a7ff36417cbee64b07ea103804a
matbalerm <matbalerm>
2022-10-05 17:56:22 +0000

    Actividad Calculadora enhanced.

--- Actividad_Calculadora.py
+++ Actividad_Calculadora.py
@@ -1,7 +1,11 @@
 # Databricks notebook source
+import math
 class calculadora:
-    def __init__(self, variables):
+    def __init__(self, variables, base = 0, exponente = 0):
         self.variables = variables
+        self.base = base
+        self.exponente = exponente
+        
     
     def suma(self):
         suma=0
@@ -30,6 +34,17 @@ class calculadora:
         for x in divej:
             div /= x
         return div
+    
+    def raiz(self, base = 0):
+        self.base = base
+        sqr = math.sqrt(self.variables[self.base])
+        return sqr
+    
+    def potencia(self, base = 0, exponente = 1):
+        self.base = base
+        self.exponente = exponente
+        potencia = self.variables[base] ** self.variables[exponente]
+        return potencia
 
 # COMMAND ----------
 
@@ -64,3 +79,19 @@ lista2.div()*calculadora.multi(lista1)
 ej1=list(range(2,30,3))
 ej1.sort(reverse=True)
 ej1
+
+# COMMAND ----------
+
+calculadora.raiz(lista1,2)
+
+# COMMAND ----------
+
+lista1.raiz(1)
+
+# COMMAND ----------
+
+calculadora.potencia(lista2)
+
+# COMMAND ----------
+
+lista1.potencia(2,0)
